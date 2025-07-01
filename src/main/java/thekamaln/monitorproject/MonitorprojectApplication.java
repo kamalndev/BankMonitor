03adc366445ec905dc9c3fd53f2f38c9c9b0a9ed
kamalndev <nallandi@usc.edu>
2025-06-30 22:55:18 -0500

    Initial commit - project setup

---
+++ src/main/java/thekamaln/monitorproject/MonitorprojectApplication.java
@@ -0,0 +1,13 @@
+package thekamaln.monitorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thekamaln/monitorproject/MonitorprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package thekamaln.monitorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
